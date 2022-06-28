95f6cfb6a5d305d013d6c5222287849d96ce1a75
lich333 <91579350+lich333@users.noreply.github.com>
2022-06-28 22:23:33 +0800

    Update healthy-rct-app.py

--- healthy-rct-app.py
+++ healthy-rct-app.py
@@ -6,7 +6,7 @@ import pickle as pkl
 from sklearn.preprocessing import MinMaxScaler
 scal=MinMaxScaler()
 #Load the saved model
-model = pkl.load(open("https://github.com/lich333/rct-classfication/blob/main/final_model.p","rb"))
+model = pkl.loads(open("final_model.p","rb"))
 
 st.set_page_config(page_title="rct App",page_icon="⚕️",layout="centered",initial_sidebar_state="expanded")
 
