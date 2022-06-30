0e3cfa49d7433f6e9eff3ba6fc3aa5f75f5b648d
lich333 <91579350+lich333@users.noreply.github.com>
2022-07-01 00:47:14 +0800

    Update healthy-rct-app.py

--- healthy-rct-app.py
+++ healthy-rct-app.py
@@ -6,13 +6,15 @@ import pickle as pkl
 from sklearn.preprocessing import MinMaxScaler
 scal=MinMaxScaler()
 import os
-
+import joblib
 
 #Load the saved model
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-model = os.path.join(APP_ROOT,'final_model.p') 
-
+model_a = os.path.join(APP_ROOT,'final_model.p') 
+with open(model_a,'rb') as file:
+   model = joblib.load(file)
 # model=pkl.load(open("final_model.p","rb"))
+
 
 
 st.set_page_config(page_title="rct App",page_icon="⚕️",layout="centered",initial_sidebar_state="expanded")
