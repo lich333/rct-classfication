b9a3b29031046ad4a77bdf03ef2f583a05d15824
lich333 <91579350+lich333@users.noreply.github.com>
2022-07-01 22:45:53 +0800

    Update healthy-rct-app.py

--- healthy-rct-app.py
+++ healthy-rct-app.py
@@ -9,12 +9,12 @@ import os
 
 
 #Load the saved model
-# APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-# model_b = os.path.join(APP_ROOT,'final_model.p') 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+pkl_path = os.path.join(APP_ROOT,'final_model.pkl') 
 # model_a = open('final_model.pkl','rb')
 # model = pkl.load(model_a)
-from pathlib import Path
-pkl_path = Path(__file__).parents[1] / 'rct-clssiction/final_model.pkl'
+
+
 xgb_pickle = open(pkl_path, 'rb')
 xgb = pickle.load(xgb_pickle)
 xgb_pickle.close()
