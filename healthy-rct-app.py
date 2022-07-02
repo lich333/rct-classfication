9076077e408cae50ff2a1a9f4ef3eb0425753d9a
lich333 <91579350+lich333@users.noreply.github.com>
2022-07-02 16:00:53 +0800

    Update healthy-rct-app.py

--- healthy-rct-app.py
+++ healthy-rct-app.py
@@ -125,75 +125,73 @@ def preprocess(sex,age,side,trauma,jobe,bear,belly,erls,ss,ISs,ssc,ases,const,uc
 
     return prediction
 
-def main():
-    # front end elements of the web page 
-   html_temp = """ 
-       <div style ="background-color:pink;padding:13px"> 
-       <h1 style ="color:black;text-align:center;">Diagnosis of RCT App</h1> 
-       </div> 
-       """
 
-   # display the front end aspect
-   st.markdown(html_temp, unsafe_allow_html = True) 
-   # following lines create boxes in which user can enter data required to make prediction
-   age=st.selectbox ("Age",range(1,121,1))
-   sex = st.radio("Select Gender: ", ('male', 'female'))
-   side=st.radio("is the painful shoulder your dominant side?", ['Yes','No'])
-   trauma=st.radio("have you get injuried in your shoulder?", ['Yes','No'])
-   jobe=st.radio("JOBE test", ['+','-'])
-   bear=st.radio("BEAR HUG", ['+','-'])
-   belly=st.radio("BELLY PRESS", ['+','-'])
-   erls=st.radio("ERLS", ['+','-'])
-   ss=st.selectbox('the strength of supraspinatus ',range(0,5,1))
-   ISs=st.selectbox('the strength of infraspinatus',range(0,5,1)) 
-   ssc=st.selectbox('the strength of subscapularis ',range(0,5,1))
-   ases=st.selectbox ("ASES",range(1,100,1))
-   const=st.selectbox ("Constant-Murley",range(1,100,1))
-   ucla=st.selectbox ("UCLA",range(1,100,1))
+ # front end elements of the web page 
+html_temp = """ 
+    <div style ="background-color:pink;padding:13px"> 
+    <h1 style ="color:black;text-align:center;">Diagnosis of RCT App</h1> 
+    </div> 
+    """
 
-   flex=st.selectbox('Flexion',("0-45°","45-90°","90-135°","135-180°"))
-   abd=st.selectbox('Abduction',("0-45°","45-90°","90-135°","135-180°"))
-   er=st.selectbox('External rotation',("0-20°","20-40°","40-60°","60-80°"))
-   ir=st.selectbox('Internal rotation',("below S1","L1-L5","above T12"))
-   vas=st.selectbox('VAS',("0-4","5-7","8-10°"))
-   time=st.selectbox('Duration of symptoms/months',("<3","3-6","6-12",">12"))
+# display the front end aspect
+st.markdown(html_temp, unsafe_allow_html = True) 
+# following lines create boxes in which user can enter data required to make prediction
+age=st.selectbox ("Age",range(1,121,1))
+sex = st.radio("Select Gender: ", ( 'female','male'))
+side=st.radio("is the painful shoulder your dominant side?", ['Yes','No'])
+trauma=st.radio("have you get injuried in your shoulder?", ['Yes','No'])
+jobe=st.radio("JOBE test", ['+','-'])
+bear=st.radio("BEAR HUG", ['+','-'])
+belly=st.radio("BELLY PRESS", ['+','-'])
+erls=st.radio("ERLS", ['+','-'])
+ss=st.selectbox('the strength of supraspinatus ',range(0,5,1))
+ISs=st.selectbox('the strength of infraspinatus',range(0,5,1)) 
+ssc=st.selectbox('the strength of subscapularis ',range(0,5,1))
+ases=st.selectbox ("ASES",range(1,100,1))
+const=st.selectbox ("Constant-Murley",range(1,100,1))
+ucla=st.selectbox ("UCLA",range(1,100,1))
 
-
-
-   # cp = st.selectbox('Chest Pain Type',("Typical angina","Atypical angina","Non-anginal pain","Asymptomatic")) 
-   # trestbps=st.selectbox('Resting Blood Sugar',range(1,500,1))
-   # restecg=st.selectbox('Resting Electrocardiographic Results',("Nothing to note","ST-T Wave abnormality","Possible or definite left ventricular hypertrophy"))
-   # chol=st.selectbox('Serum Cholestoral in mg/dl',range(1,1000,1))
-   # fbs=st.radio("Fasting Blood Sugar higher than 120 mg/dl", ['Yes','No'])
-   # thalach=st.selectbox('Maximum Heart Rate Achieved',range(1,300,1))
-   # exang=st.selectbox('Exercise Induced Angina',["Yes","No"])
-   # oldpeak=st.number_input('Oldpeak')
-
-
-   #user_input=preprocess(sex,cp,exang, fbs, slope, thal )
-   pred=preprocess(age,sex,side,trauma,jobe,bear,belly,erls,ss,ISs,ssc,ases,const,ucla,flex,abd,er,ir,vas,time)
-
-
-   if st.button("Predict"):    
-    if pred[0] == 0:
-       st.error('Warning! You have high risk of getting a rct!')
-
-    else:
-       st.success('You have lower risk of getting a rct!')
-
-
-   st.sidebar.subheader("About App")
-
-   st.sidebar.info("This web app is helps you to find out whether you are at a risk of developing a RCT.")
-   st.sidebar.info("Enter the required fields and click on the 'Predict' button to check whether you have a healthy shoulder")
-   st.sidebar.info("Don't forget to rate this app")
+flex=st.selectbox('Flexion',("0-45°","45-90°","90-135°","135-180°"))
+abd=st.selectbox('Abduction',("0-45°","45-90°","90-135°","135-180°"))
+er=st.selectbox('External rotation',("0-20°","20-40°","40-60°","60-80°"))
+ir=st.selectbox('Internal rotation',("below S1","L1-L5","above T12"))
+vas=st.selectbox('VAS',("0-4","5-7","8-10°"))
+time=st.selectbox('Duration of symptoms/months',("<3","3-6","6-12",">12"))
 
 
 
-   feedback = st.sidebar.slider('How much would you rate this app?',min_value=0,max_value=5,step=1)
+# cp = st.selectbox('Chest Pain Type',("Typical angina","Atypical angina","Non-anginal pain","Asymptomatic")) 
+# trestbps=st.selectbox('Resting Blood Sugar',range(1,500,1))
+# restecg=st.selectbox('Resting Electrocardiographic Results',("Nothing to note","ST-T Wave abnormality","Possible or definite left ventricular hypertrophy"))
+# chol=st.selectbox('Serum Cholestoral in mg/dl',range(1,1000,1))
+# fbs=st.radio("Fasting Blood Sugar higher than 120 mg/dl", ['Yes','No'])
+# thalach=st.selectbox('Maximum Heart Rate Achieved',range(1,300,1))
+# exang=st.selectbox('Exercise Induced Angina',["Yes","No"])
+# oldpeak=st.number_input('Oldpeak')
 
-   if feedback:
-     st.header("Thank you for rating the app!")
-     st.info("Caution: This is just a prediction and not doctoral advice. Kindly see a doctor if you feel the symptoms persist.") 
-if __name__=='__main__':
-    main()
+
+#user_input=preprocess(sex,cp,exang, fbs, slope, thal )
+pred=preprocess(age,sex,side,trauma,jobe,bear,belly,erls,ss,ISs,ssc,ases,const,ucla,flex,abd,er,ir,vas,time)
+
+
+if st.button("Predict"):    
+ if pred[0] == 0:
+    st.error('Warning! You have high risk of getting a rct!')
+
+ else:
+    st.success('You have lower risk of getting a rct!')
+
+
+st.sidebar.subheader("About App")
+
+st.sidebar.info("This web app is helps you to find out whether you are at a risk of developing a RCT.")
+st.sidebar.info("Enter the required fields and click on the 'Predict' button to check whether you have a healthy shoulder")
+st.sidebar.info("Don't forget to rate this app")
+
+
+
+feedback = st.sidebar.slider('How much would you rate this app?',min_value=0,max_value=5,step=1)
+
+if feedback:
+  st.header("Thank you for rating the app!")
+  st.info("Caution: This is just a prediction and not doctoral advice. Kindly see a doctor if you feel the symptoms persist.") 
