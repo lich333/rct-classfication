91e5ee6ce55b4813332f052a0bcb33d003f56d34
lich333 <91579350+lich333@users.noreply.github.com>
2022-07-27 15:16:48 +0800

    Update healthy-rct-app.py

--- healthy-rct-app.py
+++ healthy-rct-app.py
@@ -32,7 +32,7 @@ st.set_page_config(page_title="rct App",page_icon="⚕️",layout="centered",ini
 
 
 
-def preprocess(sex,age,side,trauma,jobe,bear,belly,erls,flex,abd,er,ir,ases,const,ucla,vas,time ):   
+def preprocess(sex,age,side,vas,time,trauma,flex,abd,er,ir,jobe,,erls,lift,belly,bear,ojobe,irls ):   
  
    
     if sex=="female":
@@ -61,36 +61,6 @@ def preprocess(sex,age,side,trauma,jobe,bear,belly,erls,flex,abd,er,ir,ases,cons
     if trauma=="Yes":
         trauma=1 
     else: trauma=0
-        
-    if flex=="0-45":
-        flex=0
-    elif flex == "45-90":
-        flex=1
-    elif flex == "90-135":
-        flex=2
-    else: flex = 3
-        
-    if abd=="0-45":
-        abd=0
-    elif abd == "45-90":
-        abd=1
-    elif abd == "90-135":
-        abd=2
-    else: abd = 3
-        
-    if er=="0-20":
-        er=0
-    elif er == "20-40":
-        er=1
-    elif er == "40-60":
-        er=2
-    else: er = 3
-        
-    if ir=="below S1":
-        ir=0
-    elif ir == "L1-L5":
-        ir=1
-    else: ir = 2
     
     if jobe=="+":
         jobe=1 
@@ -107,13 +77,26 @@ def preprocess(sex,age,side,trauma,jobe,bear,belly,erls,flex,abd,er,ir,ases,cons
     if erls=="+":
         erls=1 
     else: erls= 0
-        
+      
+    if ojobe=="+":
+        ojobe=1 
+    else: ojobe= 0
+    
+    if irls=="+":
+        irls=1 
+    else: irls= 0
+      
+    if lift=="+":
+        lift=1 
+    else: lift= 0
+      
+    
 
     
   
     
         
-    user_input=[sex,age,side,trauma,jobe,bear,belly,erls,flex,abd,er,ir,ases,const,ucla,vas,time ]
+    user_input=[sex,age,side,vas,time,trauma,flex,abd,er,ir,jobe,,erls,lift,belly,bear,ojobe,irls ]
     user_input=np.array(user_input)
     user_input=user_input.reshape(1,-1)
 #     user_input=scal.fit_transform(user_input)
@@ -136,26 +119,23 @@ st.markdown(html_temp, unsafe_allow_html = True)
 sex = st.radio("Select Gender: ", ( 'female','male'))
 age = st.slider("Age",min_value=0,max_value=100,step=1)
 side=st.radio("is the painful shoulder your dominant side?", ['Yes','No'])
+vas = st.slider("VAS",min_value=0,max_value=10,step=1)
+time = st.slider("Duration of symptoms, months",min_value=0,max_value=24,step=1)
 trauma=st.radio("have you get injuried in your shoulder?", ['Yes','No'])
+flex=st.sliderx('Flexion',min_value=0,max_value=180,step=10)
+abd=st.slider('Abduction',min_value=0,max_value=180,step=10)
+er=st.slider('External rotation',min_value=0,max_value=80,step=10)
+ir=st.slider('Internal rotation,= thi,butt,S 1,L5,L 3,L 1,T 11,T 9,T 7,T 5,T 3,= 0,1,2,3,4,5,6,7,8,9,10',min_value=0,max_value=10,step=1)
 jobe=st.radio("JOBE test", ['+','-'])
-bear=st.radio("BEAR HUG", ['+','-'])
-belly=st.radio("BELLY PRESS", ['+','-'])
 erls=st.radio("ERLS", ['+','-'])
-# ss=st.selectbox('the strength of supraspinatus ',range(1,6,1))
-# ISs=st.selectbox('the strength of infraspinatus',range(1,6,1)) 
-# ssc=st.selectbox('the strength of subscapularis ',range(1,6,1))
-ases = st.slider("ASES",min_value=0,max_value=100,step=1)
-const= st.slider("Constant-Murley",min_value=0,max_value=100,step=1)
-ucla = st.slider("UCLA",min_value=0,max_value=35,step=1)
+lift= st.radio("Lift-off", ['+','-'])
+belly=st.radio("BELLY PRESS", ['+','-'])
+bear=st.radio("BEAR HUG", ['+','-'])
+ojobe= st.radio("0-Jobe", ['+','-'])
+irls = st.radio("IRLS", ['+','-'])
 
 
-flex=st.selectbox('Flexion',("0-45°","45-90°","90-135°","135-180°"))
-abd=st.selectbox('Abduction',("0-45°","45-90°","90-135°","135-180°"))
-er=st.selectbox('External rotation',("0-20°","20-40°","40-60°","60-80°"))
-ir=st.selectbox('Internal rotation',("below S1","L1-L5","above T12"))
-vas=st.selectbox('VAS',("0-4","5-7","8-10"))
-time=st.selectbox('Duration of symptoms/months',("<3","3-6","6-12",">12"))
-       
+
 
 # cp = st.selectbox('Chest Pain Type',("Typical angina","Atypical angina","Non-anginal pain","Asymptomatic")) 
 # trestbps=st.selectbox('Resting Blood Sugar',range(1,500,1))
@@ -168,15 +148,15 @@ time=st.selectbox('Duration of symptoms/months',("<3","3-6","6-12",">12"))
 
 
 #user_input=preprocess(sex,cp,exang, fbs, slope, thal )
-pred=preprocess(sex,age,side,trauma,jobe,bear,belly,erls,flex,abd,er,ir,ases,const,ucla,vas,time )
+pred=preprocess(sex,age,side,vas,time,trauma,flex,abd,er,ir,jobe,,erls,lift,belly,bear,ojobe,irls )
 
 if st.button("Predict"):              
   if pred == 1:
-    st.success('Warning! You have high risk of getting rct!,The age is {}'.format(pred))
+    st.success('Warning! You have high risk of getting rct!)
     st.info("Caution: This is just a prediction and not doctoral advice. Kindly see a doctor if you feel the symptoms persist.")
    
   else:
-    st.success('You have lower risk of getting rct!,The age is {}'.format(pred))
+    st.success('You have lower risk of getting rct!)
     st.info("Caution: This is just a prediction and not doctoral advice. Kindly see a doctor if you feel the symptoms persist.") 
 
 #  else:
